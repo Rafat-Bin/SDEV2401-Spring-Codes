6530370fd5277240238cedea8ef9099fbf7f58e5
Rafat Bin <rafati@nait.ca>
2026-06-25 16:05:52 -0600

    fixed

--- Module 7/19-class-based-views-start/announcements_project/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import edit_profile, profile_list
-
-urlpatterns = [
-    path('edit/', edit_profile, name='profile_edit'),
-    path('', profile_list, name='profile_list'),
-]
--- Module 7/20-class-based-view-mixins-start/announcements_project/announcements/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AnnouncementListView, CreateAnnouncementView
-
-urlpatterns = [
-    path('', AnnouncementListView.as_view(), name='announcement_list'),
-    path('create/', CreateAnnouncementView.as_view(), name='create_announcement'),
-]
